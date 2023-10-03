b2efaf9527bdd5871f4df301b5577b5d1edc4b23
Md. Raju Ahmed <rajuahmad6789@gmail.com>
2023-10-03 13:32:27 +0600

    added nav bar

--- src/components/BagView/BagView.tsx
+++ src/components/BagView/BagView.tsx
@@ -3,6 +3,9 @@
 import { useParams } from "react-router-dom";
 import { OrderData } from "../../types/orderData";
 import StopIcon from "@mui/icons-material/Stop";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+import ScaleOutlinedIcon from "@mui/icons-material/ScaleOutlined";
 import "./bagView.css";
 
 export const BagView = () => {
@@ -10,63 +13,47 @@ export const BagView = () => {
 
   const { orderID } = useParams();
   const { data }: OrderData = require("../../packing.json");
+  console.log(data);
 
   const bags = data.order_details.bag_list;
 
   return (
-    <div className="bag-view">
-      <div className="bag-view-header">
-        <div className="bag-view-order">
-          <h2>DEV-{data.order_id}</h2>
+    <div className="container">
+      <div className="header">
+        <div className="header-title">
+          <div className="order">
+            <h2>DEV-{data.order_id}</h2>
+          </div>
+          <div className="stop">
+            <StopIcon />
+            <h2>Stop packing order</h2>
+          </div>
         </div>
-        <div className="bag-view-stop">
-          <StopIcon />
-          <h2>Stop packing order</h2>
+        <div className="header-body">
+          <div className="header-body-left">
+            <div className="assignments">
+              <AssignmentIcon />
+              <p>DEV-{data.order_id}</p>
+            </div>
+
+            <div className="employee">
+              <ShoppingBagOutlinedIcon />
+              <p>{`${data.employee} | Bag 1 of 1`}</p>
+            </div>
+
+            <div className="weight">
+              <ScaleOutlinedIcon />
+              <p>{`Weight 0g of 385g`}</p>
+            </div>
+          </div>
+
+          <div className="header-body-right">
+            <p>Packing: {data.employee}</p>
+          </div>
         </div>
       </div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <textarea name="" id="">
-        asds
-      </textarea>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <textarea name="" id="">
-        asds
-      </textarea>
 
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <textarea name="" id="">
-        asds
-      </textarea>
-
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <div>test &nbsp</div>
-      <textarea name="" id="">
-        asds
-      </textarea>
-      <div className="bag-view-footer">Footer</div>
+      <div className="footer">Footer</div>
     </div>
   );
 };
